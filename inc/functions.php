1246c494f6a2b038b7d18fca2dad73874bcdd3d6
Kevin <stradtkt22@gmail.com>
2019-02-12 17:16:58 -0500

    Added connection and projects

--- inc/functions.php
+++ inc/functions.php
@@ -1,2 +1,12 @@
 <?php
 //application functions
+
+function get_project_list() {
+    include('connection.php');
+    try {
+        return $db->query('SELECT project_id, title, category FROM projects');
+    } catch(Exception $e) {
+        echo "Error: " .$e->getMessage() . "<br/>";
+        return array();
+    }
+}
--- project_list.php
+++ project_list.php
@@ -22,7 +22,11 @@ include 'inc/header.php';
 
             <div class="form-container">
                 <ul class="items">
-
+                    <?php
+                        foreach(get_project_list() as $item) {
+                            echo "<li>" . $item["title"] . "</li>";
+                        }
+                    ?>
                 </ul>
             </div>
         </div>
